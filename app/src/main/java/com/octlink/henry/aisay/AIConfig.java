9ca177e33ef0cb73d5e8d95b07fb49c1992cddff
maqg <maquangang@gmail.com>
2017-10-13 16:14:42 +0800

    add my secret key

---
+++ app/src/main/java/com/octlink/henry/aisay/AIConfig.java
@@ -0,0 +1,6 @@
+package com.octlink.henry.aisay;
+
+public class AIConfig {
+    public static String APIKey = "72KbYwrVwao2FFOhCvh934t3";
+    public static String SecretKey = "xl0MEavASmFxZjKn1Fx3FsDqgeRGo8KI";
+}
--- app/src/main/java/com/octlink/henry/aisay/ASConfig.java
+++
@@ -1,9 +0,0 @@
-package com.octlink.henry.aisay;
-
-/**
- * Created by henry on 2017/10/12.
- */
-
-public class ASConfig {
-    private String APIKey = "";
-}

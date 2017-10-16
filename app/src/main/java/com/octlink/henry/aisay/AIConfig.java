1310e5b8f6b4ed3885d28989a095e238d7a3a917
maqg <maquangang@gmail.com>
2017-10-16 17:50:42 +0800

    for online recognize basic function importing.

--- app/src/main/java/com/octlink/henry/aisay/AIConfig.java
+++ app/src/main/java/com/octlink/henry/aisay/AIConfig.java
@@ -3,5 +3,6 @@ package com.octlink.henry.aisay;
 public class AIConfig {
     public static String APIId = "10232591";
     public static String APIKey = "72KbYwrVwao2FFOhCvh934t3";
+    public static String AppName = "你听我说";
     public static String SecretKey = "xl0MEavASmFxZjKn1Fx3FsDqgeRGo8KI";
 }
--- app/src/main/java/com/octlink/henry/aisay/MainActivity.java
+++ app/src/main/java/com/octlink/henry/aisay/MainActivity.java
@@ -6,10 +6,37 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.baidu.speech.EventListener;
+import com.baidu.speech.EventManager;
+import com.baidu.speech.EventManagerFactory;
+import com.baidu.speech.asr.SpeechConstant;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements EventListener {
+
+    protected TextView txtLog;
+    protected TextView txtResult;
+    protected Button btn;
+    protected Button stopBtn;
+    private static String DESC_TEXT = "精简版识别，带有SDK唤醒运行的最少代码，仅仅展示如何调用，\n" +
+            "也可以用来反馈测试SDK输入参数及输出回调。\n" +
+            "本示例需要自行根据文档填写参数，可以使用之前识别示例中的日志中的参数。\n" +
+            "需要完整版请参见之前的识别示例。\n" +
+            "需要测试离线命令词识别功能可以将本类中的enableOffline改成true，首次测试离线命令词请联网使用。之后请说出“打电话给张三”";
+
+    private EventManager asr;
+
+    private boolean enableOffline = false; // 测试离线命令词，需要改成true
 
     /**
      * android 6.0 以上需要动态申请权限
@@ -36,9 +63,124 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void initView() {
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        txtLog = (TextView) findViewById(R.id.txtLog);
+        btn = (Button) findViewById(R.id.btn);
+        stopBtn = (Button) findViewById(R.id.btn_stop);
+        txtLog.setText(DESC_TEXT + "\n");
+    }
+
+
+    private void loadOfflineEngine() {
+        Map<String, Object> params = new LinkedHashMap<String, Object>();
+        params.put(SpeechConstant.DECODER, 2);
+        params.put(SpeechConstant.ASR_OFFLINE_ENGINE_GRAMMER_FILE_PATH, "assets://baidu_speech_grammar.bsg");
+        asr.send(SpeechConstant.ASR_KWS_LOAD_ENGINE, new JSONObject(params).toString(), null, 0, 0);
+    }
+
+    private void unloadOfflineEngine() {
+        asr.send(SpeechConstant.ASR_KWS_UNLOAD_ENGINE, null, null, 0, 0); //
+    }
+
+    //   EventListener  回调方法
+    @Override
+    public void onEvent(String name, String params, byte[] data, int offset, int length) {
+        String logTxt = "name: " + name;
+
+
+        if (params != null && !params.isEmpty()) {
+            logTxt += " ;params :" + params;
+        }
+        if (name.equals(SpeechConstant.CALLBACK_EVENT_ASR_PARTIAL)) {
+            if (params.contains("\"nlu_result\"")) {
+                if (length > 0 && data.length > 0) {
+                    logTxt += ", 语义解析结果：" + new String(data, offset, length);
+                }
+            }
+        } else if (data != null) {
+            logTxt += " ;data length=" + data.length;
+        }
+        printLog(logTxt);
+    }
+
+    private void printLog(String text) {
+
+        text += "  ;time=" + System.currentTimeMillis() + "\n";
+        Log.i(getClass().getName(), text);
+        txtLog.append(text + "\n");
+    }
+
+    /**
+     * 测试参数填在这里
+     */
+    private void start() {
+        txtLog.setText("");
+        Map<String, Object> params = new LinkedHashMap<String, Object>();
+        String event = null;
+        event = SpeechConstant.ASR_START; // 替换成测试的event
+
+        params.put(SpeechConstant.ACCEPT_AUDIO_VOLUME, false);
+        params.put(SpeechConstant.VAD,SpeechConstant.VAD_DNN);
+        params.put(SpeechConstant.APP_KEY, AIConfig.APIKey);
+        params.put(SpeechConstant.SECRET, AIConfig.SecretKey);
+        params.put(SpeechConstant.APP_NAME, AIConfig.AppName);
+        params.put(SpeechConstant.APP_ID, AIConfig.APIId);
+
+        if (enableOffline){
+            params.put(SpeechConstant.DECODER, 2);
+        }
+
+        String json = null; //可以替换成自己的json
+        json = new JSONObject(params).toString(); // 这里可以替换成你需要测试的json
+        asr.send(event, json, null, 0, 0);
+        printLog("输入参数：" + json);
+    }
+
+    private void stop() {
+        asr.send(SpeechConstant.ASR_STOP, null, null, 0, 0); //
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initPermission();
+
+        asr = EventManagerFactory.create(this, "asr");
+        asr.registerListener(this); //  EventListener 中 onEvent方法
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                start();
+            }
+        });
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                stop();
+            }
+        });
+
+        if (enableOffline) {
+            loadOfflineEngine();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        asr.send(SpeechConstant.ASR_CANCEL, "{}", null, 0, 0);
+
+        if (enableOffline) {
+            unloadOfflineEngine();
+        }
     }
 }
